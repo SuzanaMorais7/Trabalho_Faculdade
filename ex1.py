fcb726f2f845d4601d739d5c2b1fa6feb534b0ed
SuzanaMorais7 <suzanamorais02@gmail.com>
2026-03-10 19:21:53 -0300

    Add files via upload
    Analise do exemplo 1

---
+++ ex1.py
@@ -0,0 +1,5 @@
+def verificar_primeiro(lista): 
+    if len(lista) == 0: 
+        return None 
+    return lista[0] 
+print(verificar_primeiro([1, 2, 3]))
---
+++ texto.md
@@ -0,0 +1,2 @@
+O algoritmo acessa o primeiro elemento da lista. 
+O tempode execuçao é rapido pois não depende do tamanho da lista. Complexidade é O(1)
