a00cd283e58599aff14d30e716eb5a8c513abd31
SuzanaMorais7 <suzanamorais02@gmail.com>
2026-03-10 21:26:56 -0300

    Add files via upload

---
+++ ex4.py
@@ -0,0 +1,6 @@
+def pares_com_soma(lista, alvo):
+    for i in range(len(lista)):
+        for j in range(i + 1, len(lista)):
+            if lista[i] + lista[j] == alvo:
+                print(lista[i], lista[j])
+#utiliza dois loops e irá percorrer cada elemento para verificar todos os pares possíveis.
